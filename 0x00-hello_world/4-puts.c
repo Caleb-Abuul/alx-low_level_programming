e50f1bae7c67e946a2c08b7e34d3f2e154fdda21
Caleb-Abuul <abuulcaleb@gmail.com>
2023-02-25 05:53:55 -0800

    a scriot that prints a message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,9 @@
 
 int main(void)
 {
+	// string initialization
 	char str1[] = "Programming is like building a multilingual puzzle";
 
-	puts(str1);
+	puts(str1);  // printing string to stdout
 	return (0);
 }
