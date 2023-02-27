54e74651cb31d0551878c26eddaf3b93275a027a
Caleb-Abuul <abuulcaleb@gmail.com>
2023-02-26 23:18:54 -0800

    a c file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,6 +2,6 @@
 /* entry point */
 int main(void)
 {
- printf("with proper grammar, but the outcome is a piece of art,");
- return (0);
+printf("with proper grammar, but the outcome is apiece of art,\n");
+return (0);
 }
