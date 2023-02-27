430456ca1230558edbce73a987db4cde747e1cde
Caleb-Abuul <abuulcaleb@gmail.com>
2023-02-27 03:16:46 -0800

    fixed the bug

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,19 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * more headers goes there
+ * betty style doc for function main goes there
+ * main - prints a negative or positive int
+ *
+ * Retrun: always 
+ *
+*/ 
 int main(void)
 {
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	n = (rand() - RAND_MAX) / 2;
 	/* your code goes there */
 	return (0);
 }
