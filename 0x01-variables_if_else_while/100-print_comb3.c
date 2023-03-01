f732a35c45d0966dce1b747e2b734addf968a7ce
Caleb-Abuul <abuulcaleb@gmail.com>
2023-02-28 21:13:50 -0800

    a C program file that prints the combination of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+* main - entry point
+* Description: prints a combination of two dgits
+* Return: always 0
+*/
+int main(void)
+{
+	int num_1;
+	int num_2;
+
+	for (num_1 = 0; num_1 < 10; num_1++)
+	{
+		for (num_2 = 0; num_2 < 10; num_2++)
+		{
+			putchar((num_1 % 10) + '0');
+			putchar((num_2 % 10) + '0');
+			if (num_1 == 9 && num_2 == 9)
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
