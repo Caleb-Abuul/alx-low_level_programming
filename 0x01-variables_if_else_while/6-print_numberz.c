eca259e57c93a2b33fc8ae21b71fa1460dbb4006
Caleb-Abuul <abuulcaleb@gmail.com>
2023-02-28 20:16:24 -0800

    a C program file that prints numbers suing putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - entry point
+* Description: prints numbers using putchar
+* Return: always 0
+*/
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+		putchar((number % 10) + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
