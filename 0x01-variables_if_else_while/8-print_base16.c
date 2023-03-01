1911184daef851848cdde9e1973def99fc09abc0
Caleb-Abuul <abuulcaleb@gmail.com>
2023-02-28 20:37:07 -0800

    a C program that prints base 16 numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - entry point
+* Description: prints base 16 numbers
+* Return: always 0
+*/
+int main(void)
+{
+	int number;
+	char alphabet;
+
+	for (number = 0; number < 10; number++)
+	{
+		putchar((number % 10) + '0');
+	}
+
+	for (alphabet = 'a'; alphabet <= 'f'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+
+	return (0);
+}
