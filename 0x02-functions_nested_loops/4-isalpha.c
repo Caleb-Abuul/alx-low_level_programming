23b8caa684596cc7fc715c21a7b10a39e048222a
Caleb-Abuul <abuulcaleb@gmail.com>
2023-03-01 21:26:14 -0800

    a C program that checks if a character is a letter or not

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,7 @@
 * @c: character to be tested
 * Return: 1 if letter or 0 if otherwise
 */
-int int _isalpha(int c)
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
