e98219485ffec4ca59b2b37017e709da1a78b6c9
Caleb-Abuul <abuulcaleb@gmail.com>
2023-03-03 16:05:09 -0800

    a C program that adds two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* add - adds two integers
+* @m: an integer to be added
+* @n: another integer to be added
+* Return: returns the result
+*/
+int add(int m, int n)
+{
+	return (m + n);
+}
