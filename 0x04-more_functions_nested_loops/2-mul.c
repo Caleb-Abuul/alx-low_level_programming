e43132018d54be922a5d4affe5962e4120cb5f7b
Caleb-Abuul <abuulcaleb@gmail.com>
2023-03-05 03:51:23 -0800

    prints the multiole of two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* mul - multiolies two integers
+* @a: first integer
+* @b: second integer
+* Return: the multiple of two integers
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
