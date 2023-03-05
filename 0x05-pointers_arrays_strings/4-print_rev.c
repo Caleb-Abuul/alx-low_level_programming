8e2c00ff199751fc189628d8f593f26eff11492f
Caleb-Abuul <abuulcaleb@gmail.com>
2023-03-05 13:53:10 -0800

    prints a string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,16 +5,16 @@
 */
 void print_rev(char *s)
 {
-	int i;
+	int i, j, len;
 
 	while (s[i] != '\0')
 	{
 		i++;
 	}
-	i--;
-	while (s[i] != '\0')
+	len = i;
+	for (j = len - 1; j >= 0; j--)
 	{
-		_putchar(s[i--]);
+		_putchar(s[j]);
 	}
 	_putchar('\n');
 }
