ffd2e15a997ee10111422bea800b71b3bce04956
Caleb-Abuul <abuulcaleb@gmail.com>
2023-03-05 01:47:46 -0800

    a c program that tests if a function checks whether an integer is positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* main - tests a program that
+* prints positive or negative
+* Return: always 0 (success)
+*/
+int main(void)
+{
+	int n;
+
+	n = 0;
+	positive_or_negative(n);
+	return (0);
+}
