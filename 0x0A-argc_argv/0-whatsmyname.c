75973ea584b88a10d519fe6d1a0963e6c072344d
Caleb-Abuul <abuulcaleb@gmail.com>
2023-03-10 15:50:56 -0800

    a C file that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,9 +2,12 @@
 /**
 * main - prints the name of command
 * Return: always 0 (success)
+* @argc: argument count
+* @argv: arguement vector)
 */
 int main(int argc, char *argv[])
 {
+	argc--;
 	printf("%s\n", argv[0]);
 	return (0);
 }
